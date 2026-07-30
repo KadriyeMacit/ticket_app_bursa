44dfa864da88cb0fa13aa8c642079b981b5f6f64
Kadriye Macit <kadriyemacit9655@gmail.com>
2026-07-30 19:35:17 +0300

    Add the NavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app_bursa/screens/explore_screen.dart';
+import 'package:ticket_app_bursa/screens/my_tickets_screen.dart';
 import 'package:ticket_app_bursa/screens/profile_screen.dart';
 
 void main() {
@@ -14,23 +16,47 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: HomePage(),
+      home: HomeShell(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomeShell extends StatefulWidget {
+  const HomeShell({super.key});
+
+  @override
+  State<HomeShell> createState() => _HomeShellState();
+}
+
+class _HomeShellState extends State<HomeShell> {
+  int _currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> screens = [
+      ExploreScreen(),
+      MyTicketsScreen(),
+      ProfileScreen(),
+    ];
+
     return Scaffold(
-      body: ProfileScreen(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+      body: IndexedStack(index: _currentIndex, children: screens),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: _onItemTapped,
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.explore), label: "Keşfet"),
+          NavigationDestination(
+            icon: Icon(Icons.confirmation_number_outlined),
+            label: "Biletlerim",
+          ),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profil"),
         ],
       ),
     );
