c920cf08fcee52295ebc1ee9753decd28cd4ec36
Kadriye Macit <kadriyemacit9655@gmail.com>
2026-07-30 20:00:14 +0300

    Update the EventDetailScreen

--- lib/screens/event_detail.screen.dart
+++ lib/screens/event_detail.screen.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
 
-class EventDetailScreen extends StatelessWidget {
+class EventDetailScreen extends StatefulWidget {
   const EventDetailScreen({super.key, required this.title});
 
   final String title;
+
+  @override
+  State<EventDetailScreen> createState() => _EventDetailScreenState();
+}
+
+class _EventDetailScreenState extends State<EventDetailScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Etkinlik Başlığı")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(title)],
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(widget.title, textAlign: TextAlign.center),
+              SizedBox(height: 16),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: "Adınız",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  print(_nameController.text);
+                  print(_emailController.text);
+                },
+                child: Text("Bilet Al"),
+              ),
+            ],
+          ),
         ),
       ),
     );
